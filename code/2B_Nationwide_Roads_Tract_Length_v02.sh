20fbde553fcf534103818519f37c20cce829fcdf
Zach Popp <144949240+zpopp@users.noreply.github.com>
2024-05-07 10:41:22 -0400

    Remove specific parameters from 2B_Nationwide_Roads_Tract_Length_v02.sh
    Made code directory a general name and removed BU-specific computing environment language.

--- code/2B_Nationwide_Roads_Tract_Length_v02.sh
+++ code/2B_Nationwide_Roads_Tract_Length_v02.sh
@@ -4,16 +4,13 @@
 #$ -j y         ## Merge error & output files
 #$ -pe omp 8
 
-##
-## See https://www.bu.edu/tech/support/research/system-usage/running-jobs/batch-script-examples/#MEMORY
-##     for details on how to request the appropriate amount of RAM for your job
-## Use #$ -l mem_per_core=8G for 32-64 GB.
-
 module load R/4.3.1
-Rscript /projectnb/anchor/Data_Hub/Data_Requests/ZachP/Nationwide_TIGRIS/Code/2_Nationwide_Roads_Tract_Length_v02.R $SGE_TASK_ID
+Rscript code_dir/2_Nationwide_Roads_Tract_Length_v02.R $SGE_TASK_ID
 
 ## In Terminal, cd to the directory in which this bash script is located. 
-## qsub -P climlab -t 1-3143 2B_Nationwide_Roads_Tract_Length_v02.sh
+## qsub -P project -t 1-3143 2B_Nationwide_Roads_Tract_Length_v02.sh
 ##
-## Alternatively, you can create a single "submit" script that includes the 
-## bash syntax for every single day needed in your dates of interest
+## This bash script will run as an array with an index for every US county. The index will be
+## read in by the R script 2 to identify the county to process. By processing only by county,
+## we reduce the computation time needed. Alternatively, a series of counties or states could
+## be specified directly in your R script.
